9b5ff3391930bf279729f5fcadb72be47958e82a
Pratik Makune <pratikm279@gmail.com>
2026-06-20 22:11:03 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        let seen = new Set()
+        for(let i=0; i<nums.length; i++) {
+            if(seen.has(nums[i])) return true
+
+            seen.add(nums[i])
+        }
+
+        return false
+    }
+}
